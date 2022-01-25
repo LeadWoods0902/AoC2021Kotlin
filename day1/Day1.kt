56ec8c3c67b34526ecf2c00c9c79d98a8dae087e
ledwa <L.edwards0902@gmail.com>
2022-01-25 10:10:22 +0000

    Day 1 solution, storing the depths in a txt file

---
+++ day1/Day1.kt
@@ -0,0 +1,33 @@
+package day1
+
+/*
+ *  Day1.kt
+ *  AoC2021Kotlin
+ *
+ *  Created by Louis Edwards on 2022-01-25
+ */
+
+
+import java.io.File
+import kotlin.collections.ArrayList
+
+const val TAG = "Day1 |"
+fun main() {
+    val depthsArray= ArrayList<Int>()
+    val fileHandle = File("day1/depths.txt")
+    var readText: String
+
+    var increaseCount = 0
+
+        fileHandle.forEachLine {
+            readText = it
+            depthsArray.add(readText.toInt())
+        }
+
+    for(i in 1 until depthsArray.size){
+        if(depthsArray[i] > depthsArray[i-1])
+            increaseCount+= 1
+    }
+
+    println("$TAG [${depthsArray[0]}..${depthsArray[depthsArray.size-1]}] (${depthsArray.size} terms) has $increaseCount positive depth changes.\n")
+}
